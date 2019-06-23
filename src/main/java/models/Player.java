8f61fc9c495a5f7343f9ee2a8e8df6849273d52f
Thomas <s1113113@student.hsleiden.nl>
2019-06-23 17:15:27 +0200

    sultanspalace fix

--- src/main/java/models/Player.java
+++ src/main/java/models/Player.java
@@ -32,10 +32,10 @@ public class Player implements PlayerObservable {
     public List<BonusCard> playerBonusCards = new ArrayList<>();
     public String color = "red";
 
-    public int spices = 0;
-    public int fruits = 0;
-    public int jewels = 0;
-    public int fabrics = 0;
+    public int spices = 5;
+    public int fruits = 5;
+    public int jewels = 5;
+    public int fabrics = 5;
 
 
     public int maxJewels = 2;
